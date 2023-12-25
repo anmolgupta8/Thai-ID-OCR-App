effc613a4235cbb86683f2372eef65f28dc630ed
Anmol Gupta <anmolgupta081202@gmail.com>
2023-12-25 22:32:09 +0530

    Adding require comments

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+# Anmol Gupta
+
+# Importing required libraries
 from flask import Flask, render_template, request, url_for, redirect
 from PIL import Image
 import pytesseract
@@ -8,28 +11,24 @@ import subprocess
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
+# Creating regex patterns to identify patterns from strings
+
 pattern_id_no = r'\b\d{1,2} \d{4} \d{5} \d{2} \d\b'
 pattern_name = r'\bName\b\s*(\S.*)'
 pattern_ln = r'\bLastname\b\s*(\S.*)'
 pattern_dob = r'(\d{1,2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\. \d{4})'
-date_patterns = [
-    r'(\d{1,2}[\xb0°]\s\w+\. 202\d)',  # Pattern for "24° Jul. 2020"
-    r'(\d{1,2}[\xb0°]\s\w+\. \d{4})',  # Pattern for "25° Jun. 1996"
-    r'(\d{1,2} \w+\. \d{4})',
-]
-pattern_year = r'(\d{4})'
+
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
+# Creating connection with the database
 conn = sqlite3.connect('thai_id.db')
 cursor = conn.cursor()
 cursor.execute('SELECT * FROM user_data')
 rows = cursor.fetchall()
 
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
+# Route for home
 @app.route('/')
 def index():
     # Fetch data from the database
@@ -41,18 +40,21 @@ def index():
 
     # Pass data to the template
     result = {
-        'text': {},  # You may need to update this if you have any specific data to display
+        'text': {},
         'data': rows,
     }
 
     return render_template('index.html', result=result)
 
+# Route for uploading id cards
 @app.route('/upload', methods=['POST'])
 def upload():
+    # Checking if file is not present i.e., it is empty then return error that no file is provided
     if 'file' not in request.files:
         return render_template('index.html', error='No file provided')
 
     file = request.files['file']
+    # Checking if file is empty, then again return the error
     if file.filename == '':
         return render_template('index.html', error='No selected file')
 
@@ -68,21 +70,14 @@ def upload():
         'data' : rows,
     }
 
-    print(result)
-
-    # return render_template('index.html', result=result)
+    # Redirect back to the index page
     return redirect(url_for('index'))
 
-# ... (existing code)
-
-def save_to_database(json_data):
-# Connect to SQLite database (create a new one if it doesn't exist)
-    conn = sqlite3.connect('thai_id.db')
-    # Create a cursor object
-    cursor = conn.cursor()
-
+# Creating route for delete
 @app.route('/delete', methods=['POST'])
 def delete():
+
+    # Extracting identification number and deleting it based on it as it is the primary key
     identification_number = request.form.get('identification_number')
     conn = sqlite3.connect('thai_id.db')
     cursor = conn.cursor()
@@ -91,13 +86,16 @@ def delete():
     conn.close()
     return redirect(url_for('index'))
 
+# Creating route for update
 @app.route('/update', methods=['POST'])
 def update():
+    # Getting data from the request
     identification_number = request.form.get('identification_number')
     first_name = request.form.get('first_name')
     last_name = request.form.get('last_name')
     date_of_birth = request.form.get('date_of_birth')
 
+    # Updating data by first creating the connection then executing the query
     conn = sqlite3.connect('thai_id.db')
     cursor = conn.cursor()
     cursor.execute('''
@@ -153,6 +151,8 @@ def perform_ocr(file_path):
     print("Raw Tesseract Output:")
     print(text)
 
+    # Making checks if the fields after extracting are null or not 
+    # If they are not null, extract data and put it in right place otherwise set its value to Could Not Extract
     temp1 = re.search(pattern_id_no, text)
     if(temp1) : 
         identification_no = temp1.group()
@@ -175,6 +175,7 @@ def perform_ocr(file_path):
     else :
         dob_fm = "Could Not Extract"
         
+    # Saving data as JSON
 
     data = {
         "identification_number": identification_no,
@@ -182,7 +183,6 @@ def perform_ocr(file_path):
         "last_name": last_name,
         "date_of_birth": dob_fm,
     }
-    print("Data to be saved to database:", data)
     # Save OCR result to the database
     save_to_database(data)
     return data

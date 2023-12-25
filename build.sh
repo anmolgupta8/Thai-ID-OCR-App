c005406f867741298baaaeed67f34e65790bd700
Anmol Gupta <anmolgupta081202@gmail.com>
2023-12-25 21:52:32 +0530

    Updating build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-# Install dependencies using Homebrew
-brew install leptonica
-brew install tesseract
+# Install system-level dependencies using the native package manager
+# Note: This assumes a Debian-based system; adjust as needed for macOS
+apt-get update
+apt-get install -y leptonica tesseract
+
+# Install Python dependencies
+pip install -r requirements.txt
